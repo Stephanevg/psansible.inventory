dcef59b2d1263b10a3b8ea36318c9e8a725630fb
taabake4 <kevin.bates@swisscom.com>
2024-04-11 10:26:47 +0200

    added comment based help

--- Code/Functions/public/Export-AnsibleInventory.ps1
+++ Code/Functions/public/Export-AnsibleInventory.ps1
@@ -1,3 +1,24 @@
+<#
+.SYNOPSIS
+Exports an Ansible inventory to a specified directory in the specified format.
+
+.DESCRIPTION
+This function exports an Ansible inventory to the specified directory in the specified format. It allows users to customize the output format and directory path.
+
+.PARAMETER Path
+Specifies the directory where the Ansible inventory file will be exported. This parameter is mandatory.
+
+.PARAMETER OutputType
+Specifies the format of the exported inventory file. Default is "INI". This parameter is optional.
+
+.PARAMETER Inventory
+Specifies the Ansible inventory object to be exported. This parameter is mandatory.
+
+.EXAMPLE
+Export-AnsibleInventory -Path "C:\Ansible\Inventory" -OutputType "YAML" -Inventory $MyInventory
+Exports the Ansible inventory object $MyInventory to the directory "C:\Ansible\Inventory" in YAML format.
+
+#>
 Function Export-AnsibleInventory {
     [CmdletBinding()]
     Param(
--- psansible.inventory/psansible.inventory.psd1
+++ psansible.inventory/psansible.inventory.psd1
@@ -12,7 +12,7 @@
 RootModule = 'psansible.inventory.psm1'
 
 # Version number of this module.
-ModuleVersion = '0.3.0'
+ModuleVersion = '0.2.0'
 
 # Supported PSEditions
 # CompatiblePSEditions = @()
