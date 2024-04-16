78bdbee3e0cafaa12e305c3190d7a2d960ceb7ad
Van Gulick <Stephanevg@users.noreply.github.com>
2024-04-16 13:27:36 +0200

    updated tags in build script

--- CI/03_Build.ps1
+++ CI/03_Build.ps1
@@ -82,7 +82,7 @@ Write-Host "[BUILD][START][PSD1] Adding functions to export" -ForegroundColor RE
 
 $FunctionsToExport = $PublicFunctions.BaseName
 $Manifest = Join-Path -Path $ModuleFolderPath -ChildPath "$($ModuleName).psd1"
-$Tags = @( 'PSEdition_Core','PSEdition_Desktop','pshtml','html','web' )
+$Tags = @( 'PSEdition_Core','PSEdition_Desktop','ansible','automation','controller','tower','Inventory' )
 Update-ModuleManifest -Path $Manifest -FunctionsToExport $FunctionsToExport -Tags $Tags
 
 Write-Host "[BUILD][END][MAIN PSM1] building main PSM1 " -ForegroundColor RED -BackgroundColor White
