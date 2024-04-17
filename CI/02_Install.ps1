bf77185b3419bed4488542c02429190d248a022e
Kevin Bates <31886373+bateskevin@users.noreply.github.com>
2024-04-17 14:56:01 +0200

    Update 02_Install.ps1

--- CI/02_Install.ps1
+++ CI/02_Install.ps1
@@ -2,5 +2,5 @@
 write-host "[INSTALL] Start" -ForegroundColor Red -BackgroundColor White
 write-host "[INSTALL] Install core modules" -ForegroundColor Red -BackgroundColor White
 Install-Module -Name PSScriptAnalyzer -Force  -Scope CurrentUser
-Install-Module -Name Pester -Force  -Scope CurrentUser
+Install-Module -Name Pester -Force -RequiredVersion "4.9.0" -Scope CurrentUser
 write-host "[INSTALL] End" -ForegroundColor Red -BackgroundColor White
