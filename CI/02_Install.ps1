ed2916bfa8949c4aeed1b22cefeafb1b1b87c68f
taabake4 <kevin.bates@swisscom.com>
2024-04-17 15:10:29 +0200

    adding CI to the project

--- CI/02_Install.ps1
+++ CI/02_Install.ps1
@@ -3,4 +3,5 @@ write-host "[INSTALL] Start" -ForegroundColor Red -BackgroundColor White
 write-host "[INSTALL] Install core modules" -ForegroundColor Red -BackgroundColor White
 Install-Module -Name PSScriptAnalyzer -Force  -Scope CurrentUser
 Install-Module -Name Pester -Force -RequiredVersion "4.9.0" -Scope CurrentUser
+Install-Module -Name powershell-yaml -Force -RequiredVersion "0.4.7" -Scope CurrentUser
 write-host "[INSTALL] End" -ForegroundColor Red -BackgroundColor White
